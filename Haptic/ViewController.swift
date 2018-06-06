25663b57c388282c19a0f59f84d0ef1d98cba142
Todd Laney <ToddLa@HotMail.com>
2018-06-06 06:30:06 -0700

    Initial Commit

---
+++ Haptic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Haptic
+//
+//  Created by Todd Laney on 6/6/18.
+//  Copyright © 2018 Todd Laney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
